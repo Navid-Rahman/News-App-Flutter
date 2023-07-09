d4a0d5fe474fab723a5b8c1acecaaf7cf4ba1587
Navid <navidrahman92@gmail.com>
2023-07-09 17:26:06 +0600

    Web more button added

--- lib/screens/news_details_webview.dart
+++ lib/screens/news_details_webview.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:newsapp_flutter/services/utils.dart';
+import 'package:newsapp_flutter/widgets/vertical_spacing.dart';
 
 class NewsDetailWebview extends StatefulWidget {
   const NewsDetailWebview({Key? key}) : super(key: key);
@@ -15,6 +14,75 @@ class _NewsDetailWebviewState extends State<NewsDetailWebview> {
   late InAppWebViewController inAppWebViewController;
 
   double _progress = 0;
+
+  Future<void> _showModalSheetFunction() async {
+    await showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(20),
+        ),
+      ),
+      context: context,
+      builder: (context) {
+        return Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(20),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const VerticalSpacing(20),
+
+              /// Slider bar
+              Center(
+                child: Container(
+                  height: 5,
+                  width: 35,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              const VerticalSpacing(20),
+              const Text(
+                'More Options',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                ),
+              ),
+              const Divider(
+                thickness: 2,
+              ),
+
+              const VerticalSpacing(20),
+              ListTile(
+                leading: const Icon(Icons.share),
+                title: const Text('Share'),
+                onTap: () {},
+              ),
+
+              ListTile(
+                leading: const Icon(Icons.open_in_browser),
+                title: const Text('Open in browser'),
+                onTap: () {},
+              ),
+
+              ListTile(
+                leading: const Icon(Icons.refresh),
+                title: const Text('Refresh'),
+                onTap: () {},
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +101,12 @@ class _NewsDetailWebviewState extends State<NewsDetailWebview> {
         },
         child: Scaffold(
           appBar: AppBar(
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios_new),
+            ),
             iconTheme: IconThemeData(color: color),
             backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             elevation: 0,
@@ -45,7 +119,9 @@ class _NewsDetailWebviewState extends State<NewsDetailWebview> {
             ),
             actions: [
               IconButton(
-                onPressed: () {},
+                onPressed: () async {
+                  await _showModalSheetFunction();
+                },
                 icon: const Icon(Icons.more_horiz),
               ),
             ],
