b34439524614d376ffcf2f537f690565d70d6c6b
navidrahman92 <navidrahman92@gmail.com>
2024-04-03 22:46:57 +0000

    IDX test

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Text styles for News App
+/// This is a IDX test
 TextStyle smallTextStyle = GoogleFonts.montserrat(fontSize: 15);
 TextStyle titleTextStyle =
     GoogleFonts.oswald(fontSize: 28, fontWeight: FontWeight.bold);
